cfee61ded1d0e52c59b3ad7dde7e985264584e73
zgoad1 <zqgoad@gmail.com>
2018-09-23 10:27:25 -0500

    Add ruins scene

---
+++ uhhhh tristan is fat/Assets/AnimTextureWorkaround.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTextureWorkaround : MonoBehaviour {
+
+    Material mat;
+    public Vector2 offset;
+
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<MeshRenderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mat.mainTextureOffset = offset;
+	}
+}
---
+++ uhhhh tristan is fat/Assets/PlayAnimation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Animation>().Play();
+	}
+}
